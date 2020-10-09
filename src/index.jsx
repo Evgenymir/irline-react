751767ebd46e967d4f7f1d44151e8b972d4c8b40
Evgeny <bespalove.mir@gmail.com>
2020-10-09 17:35:13 +0300

    fix app

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import rootReducers from './reducers';
 import App from './App.jsx';
 
 // eslint-disable-next-line no-underscore-dangle
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
 
 const store = createStore(
     rootReducers,
